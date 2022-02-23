1c579708a53be2e5b2d580d0f60573878dd79637
Connor <91508203+Onymbyte@users.noreply.github.com>
2022-02-23 11:56:32 -0600

    Add files via upload

---
+++ lab2_4750/lab2_4750.ino
@@ -0,0 +1,94 @@
+/*
+ * Author: Scott Boudreaux
+ */
+// initialize the library with the numbers of the interface pins
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+void setup()                                                                                     
+{
+// set up the LCD's number of columns and rows:
+  lcd.begin(16,2);
+  
+}
+
+void loop()
+{
+  int brightnessPin = 9;
+  pinMode(brightnessPin, OUTPUT);
+  analogWrite(brightnessPin, 255);
+
+  int pMatrixPin = 10;
+  pinMode(pMatrixPin, OUTPUT);
+  analogWrite(pMatrixPin, 100);
+  
+// Print a message to the LCD.
+  lcd.setCursor(3,0);  
+  lcd.print("Go Tigers!");
+  lcd.setCursor(4,1);
+  lcd.print("Go LSU!");
+  delay(2000);
+
+for (int positionCounter = 0; positionCounter < 12; positionCounter++) 
+    {
+    // scroll one position right:
+    lcd.scrollDisplayRight();
+    delay(300);
+    }
+
+// Clear display
+  lcd.clear();
+  delay(2000);
+
+  lcd.setCursor(3,0);  
+  lcd.print("Go Tigers!");
+  lcd.setCursor(4,1);
+  lcd.print("Go LSU!");
+  delay(2000);
+
+for (int positionCounter = 0; positionCounter < 12; positionCounter++) 
+    {
+    // scroll one position left:
+    lcd.scrollDisplayLeft();
+    delay(300);
+    }
+  
+
+// Clear display
+  lcd.clear();
+  delay(2000);
+
+  lcd.setCursor(3,0);  
+  lcd.print("Go Tigers!");
+  lcd.setCursor(4,1);
+  lcd.print("Go LSU!");
+  delay(2000);
+
+for (int brightnessCounter = 255; brightnessCounter > 0; brightnessCounter = brightnessCounter - 10) 
+    {
+    analogWrite(brightnessPin, brightnessCounter);
+    delay(100);
+    }
+
+  analogWrite(brightnessPin, 255);
+  lcd.setCursor(3,0);  
+  lcd.print("Go Tigers!");
+  lcd.setCursor(4,1);
+  lcd.print("Go LSU!");
+  delay(2000);
+
+ for (int pixelCounter = 100; pixelCounter < 150; pixelCounter = pixelCounter + 5) 
+    {
+    analogWrite(pMatrixPin, pixelCounter);
+    delay(150);
+    }
+
+ for (int positionCounter = 0; positionCounter < 12; positionCounter++) 
+    {
+    // scroll one position right:
+    lcd.scrollDisplayRight();
+    delay(300);
+    }
+
+    lcd.clear();
+    delay(500);
+}
