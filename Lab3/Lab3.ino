5a67f9e9dd64746fdcc26579cea2ca1d7812b033
Connor Harrington <jhar199@lsu.edu>
2022-03-09 13:20:06 -0600

    Update Lab3.ino

--- Lab3/Lab3.ino
+++ Lab3/Lab3.ino
@@ -37,7 +37,48 @@ void loop() {
 void displayStr(String newStr) {
   lcd.clear();
   lcd.setCursor(0,0);
-  lcd.print(newStr);
-  lcd.setCursor(0,1);
-  lcd.print("EE4750 Section 3");
+  if(newStr.length() <= 16){
+    lcd.print(newStr);
+    lcd.setCursor(0,1);
+    lcd.print("EE4750 Section 3");
+  }
+  else if(newStr.length() <= 40){
+    lcd.print(newStr);
+    lcd.setCursor(0,1);
+    lcd.print("EE4750 Section 3");
+    for(int i = 0; i < newStr.length();i++){
+      lcd.scrollDisplayLeft();
+      delay(200);
+    }
+  }
+  else {
+    String tempStr = newStr;
+    for(int i = 0; i < newStr.length() % 40; i++)
+      tempStr += " ";
+    for(int i = 0; i < newStr.length(); i++) {
+      if(i == 0) {
+        lcd.setCursor(0,1);
+        lcd.print("EE4750 Section 3");
+      }
+      if(i % 40 == 0) {
+        //lcd.clear();
+        lcd.setCursor(0,0);
+        Serial.println(i);
+        Serial.println(tempStr.substring(i, i+40));
+        lcd.print(tempStr.substring(i, i+40));
+      }
+      else if(i % 20 == 0){
+        lcd.setCursor(0,0);
+        Serial.println(i);
+        Serial.println(tempStr.substring(i+20, i+40));
+        lcd.print(tempStr.substring(i+20, i+40));
+        
+      }
+      lcd.scrollDisplayLeft();
+      delay(200);
+    }
+  }
+
 }
+//0000000000111111111122222222223333333333444444444455555555556666666666777777777788888888889999999999aaaaaaaaaabbbbbbbbbbcccccccc
+  
