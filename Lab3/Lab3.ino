c16f2dd679bfe6cb1d121309fce7bf905a45b0ca
Connor Harrington <jhar199@lsu.edu>
2022-03-21 20:02:09 -0500

    Add comments on Lab3

--- Lab3/Lab3.ino
+++ Lab3/Lab3.ino
@@ -3,51 +3,64 @@
 #include <EEPROM.h>
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
+
+// Global variables for string and address management
 char in_byte = 0;
 String str = "";
 int address = 0;
+
+// Function that runs on startup
 void setup() {
-  // put your setup code here, to run once:
+  // Begin LCD and Serial
   lcd.begin(16,2);
   Serial.begin(9600);
+  // Display initial line and prompt user
   Serial.println("Please write your favorite line");
   displayStr("Scott & Jameson");
 
 }
 
+//Function that loops indefinitely while board runs
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Wait for Serial port to have data
   if(Serial.available() > 0) {
-    in_byte = Serial.read();
+    in_byte = Serial.read();      // Read character from serial port
+    // Check for end of string or max character length
      if (in_byte != '\n' && str.length() < 128) {
       str += in_byte;
      }
-     else {
+     else {                       // Display string and write to EEPROM
+      int len = str.length();     // Get length of string
+
+      // Tell Serial port that the input has been received
       Serial.println("Thanks for your input. You have typed:");
       Serial.println(str);
-      int len = str.length();
       Serial.println(len);
-      Serial.flush();
+      Serial.flush();             // Flush Serial port
 
+      // Get random seed for random using analog and get random address in EEPROM
       randomSeed(analogRead(0));
       address = random(EEPROM.length() - str.length()-1);
       
+      // Write the length and string into EEPROM
       EEPROM.write(address, str.length());
       for(int i=0; i < str.length(); i++){
         EEPROM.write(address+1+i, str[i]);
       }
       str = "";
+      //Read from the EEPROM line by line
       for(int i = 0; i < len; i++) {
         char newChar = char(EEPROM.read(address+1+i));
         Serial.println(String(address+1+i) + " is the location for my input to store: " + newChar);
         str += newChar; 
       }
+      //display the stored string
       Serial.println(str);
       displayStr(str);
       delay(3000);
       
+      //Reset screen and display
       str = "";
-      
       Serial.println("Please write your favorite line");
       displayStr("Scott & Jameson");
    
@@ -55,15 +68,17 @@ void loop() {
   }
 }
 
+// Function for displaying a string to LCD string
 void displayStr(String newStr) {
+  // Clear screen and set cursor
   lcd.clear();
   lcd.setCursor(0,0);
-  if(newStr.length() <= 16){
+  if(newStr.length() <= 16){      // Display string with 16 or less characters
     lcd.print(newStr);
     lcd.setCursor(0,1);
     lcd.print("EE4750 Section 3");
-  }
-  else if(newStr.length() <= 40){
+  } 
+  else if(newStr.length() <= 40){ // 40 or less length string needs to only scroll
     lcd.print(newStr);
     lcd.setCursor(0,1);
     lcd.print("EE4750 Section 3");
@@ -71,28 +86,24 @@ void displayStr(String newStr) {
       lcd.scrollDisplayLeft();
       delay(500);
     }
-  }
-  else {
+  } 
+  else {                          // For longer strings scroll around replace begining of string while scrolling
+    // Adding some padding for printing
     String tempStr = newStr;
     for(int i = 0; i < 40 - newStr.length() % 40; i++)
       tempStr += " ";
-    //Serial.println(tempStr.length());
+    // Loop through string while scrolling and replacing
     for(int i = 0; i < newStr.length() - 16; i++) {
-      if(i == 0) {
+      if(i == 0) {                // Move cursor and set initial bottom text
         lcd.setCursor(0,1);
         lcd.print("EE4750 Section 3");
       }
-      if(i % 40 == 0) {
-        //lcd.clear();
+      if(i % 40 == 0) {           //Every 40 characters (length of buffer for top line)
         lcd.setCursor(0,0);
-        //Serial.println(i);
-        //Serial.println(tempStr.substring(i, i+40));
         lcd.print(tempStr.substring(i, i+40));
       }
-      else if(i % 20 == 0){
+      else if(i % 20 == 0){       //Every 20 characters replace the first 20 characters
         lcd.setCursor(0,0);
-        //Serial.println(i);
-        //Serial.println(tempStr.substring(i+20, i+40));
         lcd.print(tempStr.substring(i+20, i+40));
         
       }
@@ -100,7 +111,7 @@ void displayStr(String newStr) {
       delay(500);
     }
   }
-
 }
-//0000000000111111111122222222223333333333444444444455555555556666666666777777777788888888889999999999aaaaaaaaaabbbbbbbbbbcccccccc
+// Test 128 character string
+// 0000000000111111111122222222223333333333444444444455555555556666666666777777777788888888889999999999aaaaaaaaaabbbbbbbbbbcccccccc
   
